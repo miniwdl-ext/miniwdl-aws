d75d101aa1d34e3797b4ee309cab1c9b91483442
Stas  Khirman <stas@khirman.com>
2022-08-19 12:49:09 +0000

    sub-task info support

--- miniwdl_aws/cli_status.py
+++ miniwdl_aws/cli_status.py
@@ -274,9 +274,40 @@ def job_status(aws_batch, aws_logs, args):
     command = container["command"]
     cli = " ".join(command)
     print(f"command     : {cli}")
-    # extract  sub-tasks
 
-    # print list of subtasks
+    # extract  sub-tasks IDs from logs
+    task_id = []
+    logs = get_log(aws_logs, logStreamName)
+    for item in logs:
+        message = item["message"]
+        if "AWS Batch job submitted" in message:
+            substring_after = message.split("jobId:", 1)[1]
+            jobId = substring_after.split('"', 2)[1]
+            task_id.append(jobId)
+    # print list of subtask
+    if len(task_id) > 0:
+        print(f"Sub-tasks   : {len(task_id)}")
+        task_description = aws_batch.describe_jobs(jobs=task_id)
+        if "jobs" not in job_description:
+            print(f"can't find job  description for {task_id}", file=sys.stderr)
+            sys.exit(1)
+
+        tasks = task_description["jobs"]
+        for task in tasks:
+            start_time_str = (
+                datetime.utcfromtimestamp(task["createdAt"] / 1000).strftime("%Y-%m-%d %H:%M:%S")
+                if "createdAt" in task
+                else "----"
+            )
+            stop_time_str = (
+                datetime.utcfromtimestamp(task["stoppedAt"] / 1000).strftime("%Y-%m-%d %H:%M:%S")
+                if "stoppedAt" in task
+                else "----"
+            )
+            print(
+                f"{task['jobId']}\t{task['status']}\t{start_time_str}\t{stop_time_str}"
+                f"\t{task['jobName']}"
+            )
 
 
 def get_log(aws_logs, logStreamName):
